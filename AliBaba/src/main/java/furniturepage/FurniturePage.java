c69af99776143ea4c691b4b9863e8dcb83baa54a
sam2insane <49218809+sam2insane@users.noreply.github.com>
2022-01-05 17:54:13 -0500

    Update FurniturePage.java

--- AliBaba/src/main/java/furniturepage/FurniturePage.java
+++ AliBaba/src/main/java/furniturepage/FurniturePage.java
@@ -7,76 +7,35 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import reporting.TestLogger;
-public class FurniturePage extends CommonAPI {
-   @FindBy(xpath = "/html/body/div[2]/div/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[2]/div[7]/a/div[1]")
-    WebElement HomeFurniture;
-   @FindBy(xpath = "/html/body/div[2]/div/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[2]/div[5]/a/div[1]")
-   WebElement Functionalfurniture;
-   @FindBy(xpath = "/html/body/div[2]/div/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[2]/div[3]/a/div[1]")
-   WebElement commercialfurniture;
-   @FindBy(xpath = "/html/body/div[2]/div/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[2]/div[2]/a/div[1]")
-   WebElement childrenfurniture;
-   @FindBy(xpath = "//*[@id=\"J_SC_header\"]/header/div[2]/div[2]/div/div/form/div[2]/input")
-   WebElement search;
-   @FindBy(xpath = "//*[@id=\"J_SC_header\"]/header/div[2]/div[3]/div[2]/div[1]/div/a")
-   WebElement order;
-   @FindBy (xpath = "")
-   WebElement DropDown;
-   @FindBy (xpath = "//*[@id=\"J_SC_header\"]/header/div[2]/div[1]/div/div/div/ul/li[5]/div[1]/a[2]")
-   WebElement Lights;
+public int solution(int N, string S) {
+   int number = 1;
+   int counter = 0;
+   List listReserved = new List();
+   listReserved = S.Split(' ').ToList();
+   while (number <= N) {
+      if (listReserved.Where(x => x.Contains(number + "B") || x.Contains(number + "C") || x.Contains(number + "D") || x.Contains(number + "E")).FirstOrDefault() == null) {
+         listReserved.AddRange(new List {
+            number + "B", number + "C", number + "D", number + "E"
+         });
+         counter++;
+      }
 
-   public void setOrder() {
-       TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-       order.click();
-    }
-   public void setSearch(String name){
-       TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-       if(search.isEnabled())
-           Assert.assertTrue(true);
-       search.sendKeys(name, Keys.ENTER);
-   }
-   public void setChildrenfurniture(){
-       TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-       if(childrenfurniture.isDisplayed())
-           childrenfurniture.click();
-   }
-   public void setCommercialfurniture(){
-       TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-       if(commercialfurniture.isDisplayed())
-       commercialfurniture.click();
-   }
-   public void setFunctionalfurniture(){
-       TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-       if(Functionalfurniture.isDisplayed())
-           Functionalfurniture.click();
-   }
-   public void setHomeFurniture() {
-       TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-       if (HomeFurniture.isDisplayed())
-           HomeFurniture.click();
-   }
-   public void setHoverToCategories() {
-       TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-       clickByXpath("//*[@id=\"J_SC_header\"]/header/div[2]/div[1]/div/div/h3/span");
-       sleepFor(2);
-       clickByXpath("//*[@id=\"J_SC_header\"]/header/div[2]/div[1]/div/div/div/a");
-   }
-   public void CheckMyMessages() {
-       TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-      clickByXpath("//*[@id=\"J_SC_header\"]/header/div[2]/div[3]/div[1]/div[1]/div[3]/a");
+      if (listReserved.Where(x => x.Contains(number + "F") || x.Contains(number + "G") || x.Contains(number + "H") || x.Contains(number + "J")).FirstOrDefault() == null) {
+         listReserved.AddRange(new List {
+            number + "F", number + "G", number + "H", number + "J"
+         });
+         counter++;
+      }
 
+      if (listReserved.Where(x => x.Contains(number + "D") || x.Contains(number + "E") || x.Contains(number + "F") || x.Contains(number + "G")).FirstOrDefault() == null) {
+         listReserved.AddRange(new List {
+            number + "D", number + "E", number + "F", number + "G"
+         });
+         counter++;
+      }
+
+      number++;
    }
 
-    public void setChildrenfurniture(WebElement childrenfurniture) {
-        this.childrenfurniture = childrenfurniture;
-    }
-    public void DropDownTest() {
-       mouseHoverByXpath("//*[@id=\"J_SC_header\"]/header/div[2]/div[1]/div/div/h3/i[1]");
-       if (Lights.isDisplayed())
-           Lights.getText();
-    }
-    public void DropDownInfo() {
-
-
-    }
+   return counter;
 }
